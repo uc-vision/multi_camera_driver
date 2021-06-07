84b62166a4faa8e475245440665ab0d749a530c4
Robot Arm <arm@conan.canterbury.ac.nz>
2021-06-07 16:43:05 +1200

    MOve some parts to stereo_inference and camera_geometry_ros

--- src/spinnaker_camera_driver_helpers/common.py
+++ src/spinnaker_camera_driver_helpers/common.py
@@ -56,128 +56,6 @@ def load_config(config_file):
         return None
 
 
-def defer(f, args):
-    thread = Thread(target=f, args=args)
-    thread.start()
-    return thread
-
-
-def equal_pair(pair, left, right):
-    return pair.cameras[0].approx_eq(left) and pair.cameras[1].approx_eq(right)
-
-
-class StereoPublisher(object):
-    def __init__(self, name, left, right, resize=None, queue_size=1, encoding='bgr8', publish_transform=True):
-
-        self.buffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.buffer)
-
-        self.frames = (left, right)
-        self.name = name
-
-        self.publish_transform = publish_transform
-
-        self.pair = None
-        self.bridge = CvBridge()
-
-        self.resize = resize
-        self.encoding = encoding
-
-        info_topics = ["{}/camera_info".format(camera)
-                       for camera in [left, right]]
-        image_topics = ["{}/image_raw".format(camera)
-                        for camera in [left, right]]
-
-        self.stereo_publisher = rospy.Publisher("{}/stereo_info".format(self.name),
-                                                StereoCameraInfo, queue_size=queue_size)
-
-        self.stereo_full_publisher = rospy.Publisher("{}/stereo_info_full".format(self.name),
-                                                     StereoCameraInfo, queue_size=queue_size)
-
-        self.image_publishers = [RawPublisher("{}/{}".format(self.name, camera), image_topic="image_color_rect", encoding=self.encoding,
-                                              queue_size=queue_size) for camera in ["left", "right"]]
-
-        image_subscribers = []
-
-        image_subscribers = [message_filters.Subscriber(
-            topic, Image, queue_size=queue_size, buff_size=2**24) for topic in image_topics]
-        info_subscribers = [message_filters.Subscriber(
-            topic, CameraInfo, queue_size=queue_size) for topic in info_topics]
-
-        sync = message_filters.TimeSynchronizer(
-            info_subscribers + image_subscribers, queue_size=1)
-        sync.registerCallback(self.frame_callback)
-
-        self.broadcaster = tf2_ros.StaticTransformBroadcaster()
-
-    def decode_rectify(self, image_msg, calibration):
-        image = self.bridge.imgmsg_to_cv2(
-            image_msg, desired_encoding=self.encoding)
-        source_size = (image.shape[1], image.shape[0])
-
-        if source_size != self.pair.image_size:
-            image = cv2.resize(image, self.pair.image_size,
-                               interpolation=cv2.INTER_AREA)
-
-        return calibration.rectify(image)
-
-    def frame_callback(self, left_info, right_info, left_msg, right_msg):
-        timestamp = left_info.header.stamp
-        try:
-            msg = self.buffer.lookup_transform(
-                self.frames[0], self.frames[1], rospy.Time())
-
-            transform = conversions.transform_from_msg(msg)
-
-            left = conversions.camera_from_msg(left_info)
-            right = conversions.camera_from_msg(
-                right_info, parent_to_camera=transform.transform)
-
-            # For logging purposes - output full size stereo_info
-            pair_full = rectify_pair(left, right)
-            stereo_info_full = stereo_info_msg(pair_full)
-
-            stereo_info_full.header = make_header(self.name,  timestamp)
-            self.stereo_full_publisher.publish(stereo_info_full)
-
-            if self.resize is not None:
-                left = left.resize_image(self.resize)
-                right = right.resize_image(self.resize)
-
-            if self.pair is None or (not equal_pair(self.pair, left, right)):
-                rospy.loginfo(f"StereoPublisher: found new calibration for {self.frames}")
-                self.pair = rectify_pair(left, right)
-
-                # Broadcast the rectification induced rotation as a transform
-                if self.publish_transform:
-                    msg = conversions.transform_msg(
-                        transform, self.frames[0], self.name + "/left", timestamp)
-                    self.broadcaster.sendTransform(msg)
-
-            stereo_info = stereo_info_msg(self.pair)
-
-            stereo_info.header = make_header(self.name,  timestamp)
-            self.stereo_publisher.publish(stereo_info)
-
-            for publisher, image_msg, calibration in zip(self.image_publishers, [left_msg, right_msg], self.pair.rectified):
-                image = self.decode_rectify(image_msg, calibration)
-
-                cam_info = conversions.rectified_info_msg(calibration)
-                publisher.publish(image, timestamp, cam_info)
-
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            rospy.logwarn(f"Could not lookup transform: {e}")
-            pass
-
-
-def make_header(frame_id, timestamp, seq=0):
-    header = Header()
-    header.stamp = timestamp
-    header.frame_id = frame_id
-    header.seq = seq
-    return header
-
-
 class Lazy(object):
     def __init__(self, f, *args, **kwargs):
         self.f = partial(f, *args, **kwargs)
@@ -238,7 +116,7 @@ class RawPublisher(rospy.SubscribeListener):
             publisher.publish(image_msg)
 
     def publish(self, image, timestamp, cam_info=None):
-        header = make_header(self.name, timestamp, self.seq + 1)
+        header = Header(frame_id=self.name, stamp=timestamp, seq=self.seq + 1)
 
         cam_info = cam_info or CameraInfo()
         cam_info.header = header
@@ -316,7 +194,7 @@ class ImagePublisher(rospy.SubscribeListener):
 
     def publish(self, image, timestamp, cam_info=None):
 
-        header = make_header(self.name, timestamp, self.seq + 1)
+        header = Header(frame_id=self.name, stamp=timestamp, seq=self.seq + 1)
 
         cam_info = cam_info or CameraInfo()
         cam_info.header = header
